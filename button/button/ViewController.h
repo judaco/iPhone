9c36308a9e2a20b11ec97a1f4a2d092c6f2682bb
judaco <judacossa@gmail.com>
2017-04-18 21:30:57 +0300

    Button
    Button

---
+++ button/button/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  button
+//
+//  Created by Juda Cossa on 18/04/2017.
+//  Copyright © 2017 Juda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ button/button/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  button
+//
+//  Created by Juda Cossa on 18/04/2017.
+//  Copyright © 2017 Juda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
